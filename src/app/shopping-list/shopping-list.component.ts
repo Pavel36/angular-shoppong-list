0971cd86beb5a29d97b4663fcb886489224730b4
pavel <pgocev36@gmail.com>
2022-03-17 12:53:10 +0300

    Add edit section.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Meat', 128),
+    new Ingredient('Apples', 12),
+    new Ingredient('Tomatoes', 18),
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
